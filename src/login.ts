399deded46fbcbdfe2dac93e653e9efd5b3b2f88
vanaigr <vanaigranov@gmail.com>
2026-01-02 13:11:08 -0600

    initial commit

---
+++ src/login.ts
@@ -0,0 +1,26 @@
+import puppeteer from 'puppeteer'
+import fs from 'node:fs'
+import * as L from './lib/log.ts'
+
+const log = L.makeLogger('log.txt')
+
+const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--no-sandbox'],
+})
+const page = await browser.newPage()
+await page.goto('https://linkedin.com')
+try {
+    await new Promise(resolve => page.once('close', resolve))
+}
+catch(err) {
+    log.E([err])
+    log.E('Got error. Will still get cookies')
+}
+
+const cookies = await browser.cookies()
+log.I('Saving cookies')
+
+fs.writeFileSync('data/cookies.txt', JSON.stringify(cookies))
+
+await browser.close()
